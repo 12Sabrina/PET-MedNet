367c077da245e08a8eeda87b3bf97897a77bfbbc
苏奕羽 <156072059+12Sabrina@users.noreply.github.com>
2025-12-02 21:29:25 +0800

    Delete MedNet_Classification directory

--- MedNet_Classification/data/__init__.py
+++
@@ -1,4 +0,0 @@
-# 数据模块初始化文件
-from .dataset import TCIADataset, create_data_loaders, create_sample_csv
-
-__all__ = ['TCIADataset', 'create_data_loaders', 'create_sample_csv']
--- MedNet_Classification/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# 初始化文件
-from .mednet import mednet18, mednet34, mednet50, MedNet
-
-__all__ = ['mednet18', 'mednet34', 'mednet50', 'MedNet']
